f406344e2bae8737da98355a66af37cc1fe848cd
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-21 17:59:18 +0000

    001: Initial Commit

--- experiments/OLID/olid_train.py
+++ experiments/OLID/olid_train.py
@@ -5,4 +5,4 @@ from offensive_nn.offensive_nn_model import OffensiveNNModel
 olid_train = pd.read_csv('experiments/OLID/olid_train.csv', sep="\t")
 olid_test = pd.read_csv('experiments/OLID/olid_test.csv', sep="\t")
 
-model = OffensiveNNModel(model_type="lstm", embedding_model_name="word2vec-google-news-300")
+model = OffensiveNNModel(model_type="lstm", embedding_model_name="word2vec-google-news-300", train_df=olid_train)
