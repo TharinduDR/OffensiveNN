6d2d8c9ae7743061f1da951c9381906e3bc2072d
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-22 15:48:05 +0000

    001: Initial Commit

--- offensive_nn/models/offensive_lstm_model.py
+++ offensive_nn/models/offensive_lstm_model.py
@@ -1,7 +1,5 @@
 from tensorflow.python.keras import Input, Model
-from tensorflow.python.keras.layers import Embedding, Bidirectional, LSTM, Dense
-
-from offensive_nn.models.layers import Attention
+from tensorflow.python.keras.layers import Embedding, Bidirectional, LSTM, Dense, Attention
 
 
 class OffensiveLSTMModel:
@@ -10,7 +8,7 @@ class OffensiveLSTMModel:
         x = Embedding(args.max_features, args.embed_size, weights=[embedding_matrix], trainable=False)(inp)
         x = Bidirectional(LSTM(64, return_sequences=True))(x)
         x = Bidirectional(LSTM(64, return_sequences=True))(x)
-        x = Attention(args.max_len)(x)
+        # x = Attention(args.max_len)(x)
         x = Dense(256, activation="relu")(x)
         # x = Dropout(0.25)(x)
         x = Dense(args.num_classes, activation="sigmoid")(x)
