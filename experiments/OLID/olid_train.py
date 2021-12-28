00ba9ab73e4787723507f5cef155b1a0a51cb736
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-28 16:47:26 +0000

    001: Initial Commit

--- experiments/OLID/olid_train.py
+++ experiments/OLID/olid_train.py
@@ -3,9 +3,12 @@ from sklearn.model_selection import train_test_split
 
 from experiments.OLID.olid_config import args
 from offensive_nn.offensive_nn_model import OffensiveNNModel
+from offensive_nn.util.label_converter import encode
 
 olid_train = pd.read_csv('experiments/OLID/olid_train.csv', sep="\t")
 olid_test = pd.read_csv('experiments/OLID/olid_test.csv', sep="\t")
+
+olid_train['Class'] = encode(olid_train["Class"])
 
 
 olid_train, olid_validation = train_test_split(olid_train, test_size=0.2)
