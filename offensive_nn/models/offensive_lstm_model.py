616bc818ec77c223825c8e16f98706ab18ac2b92
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-29 12:16:08 +0000

    001: Initial Commit

--- offensive_nn/models/offensive_lstm_model.py
+++ offensive_nn/models/offensive_lstm_model.py
@@ -6,11 +6,12 @@ from tensorflow.keras import layers
 class OffensiveLSTMModel:
 
     def __init__(self, args, embedding_matrix):
-        inp = tf.keras.Input(shape=(None,), dtype="int64")
-        x = layers.Embedding(args.max_features, args.embed_size, embeddings_initializer=keras.initializers.Constant(embedding_matrix), trainable=False)(inp)
-        x = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(x)
-        x = layers.Bidirectional(layers.LSTM(64))(x)
-        x = layers.Dense(256, activation="relu")(x)
-        x = layers.Dense(args.num_classes, activation="softmax")(x)
-        self.model = tf.keras.Model(inputs=inp, outputs=x)
-        # self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        inp = tf.keras.Input(shape=(None,), dtype="int64", name="input")
+        x = layers.Embedding(args.max_features, args.embed_size,
+                             embeddings_initializer=keras.initializers.Constant(embedding_matrix), trainable=False,
+                             name="embedding_layer")(inp)
+        x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, name="lstm_1"))(x)
+        x = layers.Bidirectional(layers.LSTM(64, name="lstm_2"))(x)
+        x = layers.Dense(256, activation="relu", name="dense_1")(x)
+        x = layers.Dense(args.num_classes, activation="softmax", name="dense_predictions")(x)
+        self.model = tf.keras.Model(inputs=inp, outputs=x, name="lstm_model")
