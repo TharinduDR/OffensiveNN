d2e3f2c5f284c5a5cbff164533c916487a1e2df2
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-29 22:30:17 +0000

    001: Initial Commit

--- experiments/OLID/olid_train.py
+++ experiments/OLID/olid_train.py
@@ -31,7 +31,8 @@ final_predictions = []
 for row in test_preds:
     row = row.tolist()
     final_predictions.append(int(max(set(row), key=row.count)))
-    olid_test['predictions'] = final_predictions
+
+olid_test['predictions'] = final_predictions
 
 olid_test['predictions'] = decode(olid_test['predictions'])
 print_information(olid_test, "predictions", "Class")
