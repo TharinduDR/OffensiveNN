2db4b4f685dc2734ab2658e7f804787d2ba8ac86
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-30 12:26:58 +0000

    001: Initial Commit

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="offensivenn",
-    version="1.1.1",
+    version="0.0.1",
     author="Tharindu Ranasinghe",
     author_email="rhtdranasinghe@gmail.com",
     description="Offensive Language Identification with Neural Networks",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/TharinduDR/https://github.com/TharinduDR/OffensiveNN",
-    packages=find_packages(exclude=("examples", "docs", )),
+    packages=find_packages(exclude=("experiments", )),
     classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
@@ -21,7 +21,6 @@ setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        "tensorflow"
         "pandas"
         "numpy"
         "gensim"
