87ae410cfba34ae12eab87d46a31b54d06ac6842
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-30 22:51:05 +0000

    001: Initial Commit

--- offensive_nn/models/offensive_lstm_model.py
+++ offensive_nn/models/offensive_lstm_model.py
@@ -6,10 +6,11 @@ from tensorflow.keras import layers
 class OffensiveLSTMModel:
 
     def __init__(self, args, embedding_matrix):
-        inp = tf.keras.Input(shape=(None,), dtype="int64", name="input")
         emb = layers.Embedding(args.max_features, args.embed_size, trainable=False,
-                             name="embedding_layer")(inp)
-        x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, name="lstm_1"))(emb)
+                               name="embedding_layer")
+        inp = tf.keras.Input(shape=(None,), dtype="int64", name="input")
+        x = emb(inp)
+        x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, name="lstm_1"))(x)
         x = layers.Bidirectional(layers.LSTM(64, name="lstm_2"))(x)
         x = layers.Dense(256, activation="relu", name="dense_1")(x)
         x = layers.Dense(args.num_classes, activation="softmax", name="dense_predictions")(x)
